a68162e321699391f520ffcef000555fb1bdfd74
Louis-Vu <hoangvuhuy04@gmail.com>
2023-08-21 12:18:41 +0700

    exam
    exam

---
+++ EXAMC#/ExammC/ExammC/Program.cs
@@ -0,0 +1,40 @@
+﻿using exam.Controller;
+using System;
+
+ProductController productController = new ProductController();
+
+int choose = 0;
+
+try
+{
+    do
+    {
+        Console.WriteLine("---- Product Management Menu ----");
+        Console.WriteLine("1.Add Product");
+        Console.WriteLine("2.Display all products");
+        Console.WriteLine("3.Delete product by Id");
+        Console.WriteLine("4.Exit");
+
+        choose = int.Parse(Console.ReadLine());
+
+        switch (choose)
+        {
+            case 1:
+
+                productController.AddProduct();
+                break;
+            case 2:
+                productController.DisplayAllProduct();
+                break;
+            case 3:
+                productController.DeleteProductById();
+                break;
+            case 4:
+                return;
+        }
+    } while (true);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
